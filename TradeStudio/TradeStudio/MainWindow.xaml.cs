551912284d3521f5ecb5a93edef6b281dae72e78
Timofeev Andrey <yazy4nik@gmail.com>
2012-12-06 17:12:01 +0400

    test

--- TradeStudio/TradeStudio/MainWindow.xaml.cs
+++ TradeStudio/TradeStudio/MainWindow.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using StockSharp.Quik;
 
 namespace TradeStudio
 {
@@ -22,6 +12,12 @@ namespace TradeStudio
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            QuikTrader q = new QuikTrader(QuikTerminal.GetDefaultPath());
+            q.Connect();
         }
     }
 }
